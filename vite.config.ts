19d092f3b6c30876c00571f52c156b2690996e03
Akiba, S <anosatsuk124@gmail.com>
2024-11-18 00:43:03 +0000

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import wasm from 'vite-plugin-wasm';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [wasm()],
+  plugins: [wasm(), react()],
   build: {
     lib: {
       entry: './lib/index.ts',
